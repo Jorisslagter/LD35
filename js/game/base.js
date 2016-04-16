353143d15aedd94e5312598c13cc7bf1e8a52034
Joris <micra.online@gmail.com>
2016-04-16 13:52:01 +0200

    Removed the folders

--- js/game/base.js
+++
@@ -1,8 +0,0 @@
-define([''], function () {
-    var Base = function(x, y) {
-
-
-    }
-
-    return Base;
-});
--- js/game/tower.js
+++
@@ -1,8 +0,0 @@
-define([], function () {
-    var Tower = function(x, y) {
-    
-
-    }
-
-    return Tower;
-});
