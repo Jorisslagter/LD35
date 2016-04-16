58f4ebf1ef56e928301b5f2de0bb52ba9fa9d4bf
Joris <micra.online@gmail.com>
2016-04-16 13:20:16 +0200

    Added abstractions for tower and base

---
+++ js/game/base.js
@@ -0,0 +1,8 @@
+define([''], function () {
+    var Base = function(x, y) {
+
+
+    }
+
+    return Base;
+});
---
+++ js/game/tower.js
@@ -0,0 +1,8 @@
+define([], function () {
+    var Tower = function(x, y) {
+    
+
+    }
+
+    return Tower;
+});
